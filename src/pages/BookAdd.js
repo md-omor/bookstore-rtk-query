d2e639fdd93ebc5b928132c17ba8299f8e9e2c1d
Md Omor <developeromor@gmail.com>
2023-03-20 19:05:07 +0600

    books add done

---
+++ src/pages/BookAdd.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AddBook from "../components/add/AddBook";
+
+const BookAdd = () => {
+  return (
+    <>
+      <AddBook />
+    </>
+  );
+};
+
+export default BookAdd;
