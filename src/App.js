a1f3819018e919af6ca89f51cc648678bd2a289d
Md Omor <developeromor@gmail.com>
2023-03-22 19:01:30 +0600

    initialize the app

--- src/App.js
+++ src/App.js
@@ -1,58 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import BookAdd from "./pages/BookAdd";
+import BookEdit from "./pages/BookEdit";
+import Home from "./pages/Home";
 
-function App() {
+const App = () => {
+  const [inputText, setInputText] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <Router>
+      <Navbar inputText={inputText} setInputText={setInputText} />
+      <Routes>
+        <Route path="/" element={<Home inputText={inputText} />} />
+        <Route path="/books/add" element={<BookAdd />} />
+        <Route path="/books/edit/:bookId" element={<BookEdit />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
